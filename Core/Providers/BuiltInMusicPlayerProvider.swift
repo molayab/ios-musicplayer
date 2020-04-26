0bbf7dbad7a840a8f921a6ddd9ef47830af89c26
Mateo Olaya <dev.mateoolaya@gmail.com>
2020-04-26 15:50:13 -0500

    Added BuiltInMusicPlayerProvider base file.

---
+++ Core/Providers/BuiltInMusicPlayerProvider.swift
@@ -0,0 +1,11 @@
+//
+//  BuiltInMusicPlayerProvider.swift
+//  Core
+//
+//  Created by Mateo Olaya Bernal on 26/04/20.
+//
+
+import Foundation
+
+public final class BuiltInMusicPlayerProvider/*: Player */ {
+}
