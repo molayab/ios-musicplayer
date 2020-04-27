97abbc6e7cd898a316ce057c3db5128ab92708f8
Mateo Olaya <dev.mateoolaya@gmail.com>
2020-04-26 19:19:51 -0500

    Added theme, colors and fonts to UI module.

---
+++ UI/Theme.swift
@@ -0,0 +1,30 @@
+//
+//  Theme.swift
+//  UI
+//
+//  Created by Mateo Olaya Bernal on 26/04/20.
+//
+
+import UIKit
+
+public final class Theme {
+    public enum Colors: String {
+        case backgroundPrimaryColor
+        case backgroundSecundaryColor
+        case redTintColor
+
+        public var colorized: UIColor {
+            return UIColor(named: rawValue) ?? .black
+        }
+    }
+
+    public struct Fonts {
+        
+    }
+
+    public let colors = Colors()
+    public let fonts = Fonts()
+
+    public static let `default` = Theme()
+    private init() { }
+}
